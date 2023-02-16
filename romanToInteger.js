07ba004578a2c124aa10849f65140f14ceba4f75
María Alejandra Boada Rodríguez <mboada@valledelcauca.gov.co>
2023-02-16 17:20:26 -0500

    Desarrollo de la logica del archivo romanToInteger

---
+++ romanToInteger.js
@@ -0,0 +1,25 @@
+/**
+ * @param {string} s
+ * @return {number}
+ */
+var romanToInt = function(s) {
+    const romanList = {
+        I: 1,
+        V: 5,
+        X: 10,
+        L: 50,
+        C: 100,
+        D: 500,
+        M: 1000
+    }
+    
+    inputArray = s.slice('')
+    for(let i=0; i<inputArray.length;i++){
+        let number = 0
+        
+        
+    }
+    
+};
+
+romanToInt('MCMXCIV')
