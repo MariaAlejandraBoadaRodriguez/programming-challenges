6fedc6a5c2d03f704ec6358aee0a14e6ab395e03
Alejandra <alejandrarodriguez2508@gmail.com>
2023-02-17 21:19:37 -0500

    Intento 17 de febrero 2023 - El problema es el ultimo numero

--- romanToInteger.js
+++ romanToInteger.js
@@ -22,19 +22,32 @@ var romanToInt = function(s) {
         array.push(romanList[claveAleatoria])
     }
 
-    for (let i = 0; i < array.length - 1; i++) { 
-        if (array[i] < array[i+1]) {
-            numero = numero + (array[i+1] - array[i]);
-        }else {
+    for (let i = 0; i < array.length - 1; i++) {
+        if (array[i] > array[i+1]) {
             numero = numero + array[i];
+        }else if (array[i] === array[i+1]) {
+          numero = numero + array[i]
+        }else { 
+            numero = numero +(array[i+1] - array[i])
+            i++
         }
-        console.log(numero)
     }
-    console.log("return "+numero)
-    
-    
-    
-    
+        if (array[array.length - 1] === array[array.length - 2]) {
+            numero = numero + array[array.length - 1]
+        }
+    console.log(numero)
+    return numero
 };
 
-romanToInt('MCMXCIV')
+romanToInt('DCXXI')
+
+/*
+    
+III         2 / 2P   0 = 1    1 + 1  
+LVIII      57 / 4P   0 > 1    50     --> 1 > 2 =  5 + 50 = 55 --> 2 = 3  es 
+MCMXCIV  1994 / 6P   
+DCXXI    D > C   500 > 100 = 500
+         C > X  100 > 10  = 500 + 100 = 600
+         X = X   10 = 10  = 600 + 10 = 610
+         X > I   10 > 1   = 610 + 10 = 620
+*/ 
